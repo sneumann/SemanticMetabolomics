89f6bd90b3d7c75766169c25ab0a2358b2256042
ermueller <erik@east.de>
2014-06-09 18:14:05 +0200

    Example R-file for queries added

--- Exampletriples/Queries.R
+++ Exampletriples/Queries.R
@@ -1,2 +1,41 @@
 library(rrdf)
-store <- load.rdf("Queries.R")
+store <- load.rdf("example.ttl", format="TURTLE")
+add.prefix(store,
+	prefix="mbco",
+	namespace="http://www.ipb-halle.de/ontology/mbco#"
+)
+
+add.prefix(store,
+	prefix="ov",
+	namespace="http://bio2rdf.org/obo_vocabulary:"
+)
+
+add.prefix(store,
+	prefix="brdf",
+	namespace="http://bio2rdf.org/"
+)
+
+sparql.rdf(store, paste(
+	"SELECT ?subject ?object {",
+	" ?subject <http://www.ipb-halle.de/ontology/mbco#is-peak-of> ?object",
+	"}"
+))
+
+sparql.rdf(store, paste(
+	"SELECT ?s ?o WHERE{",
+	" ?s <http://www.ipb-halle.de/ontology/mbco#contains-peak> ?o.",
+	" ?o <http://www.ipb-halle.de/ontology/mbco#mz> \"147.044\"",
+	"}"
+))
+
+sparql.rdf(store, paste(
+	"SELECT ?object WHERE{",
+	" ?object <http://www.ipb-halle.de/ontology/mbco#mz> \"147.044\"",
+	"}"
+))
+
+sparql.rdf(store, paste(
+	"SELECT ?subject ?object WHERE{",
+	" ?subject <http://bio2rdf.org/obo_vocabulary:x-chebi> ?object",
+	"}"
+))

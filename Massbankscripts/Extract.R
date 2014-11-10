e500f633b88c5ea8919042ca1b8e6e266fcab67b
ermueller <erik@east.de>
2014-11-10 13:24:56 +0100

    Clean-up and added new versions of the graphs and .ttl ontology

--- Massbankscripts/Extract.R
+++ Massbankscripts/Extract.R
@@ -11,37 +11,41 @@ EXTRACT <- function(record){
 	
 	##CLASSES (except peak, comes later)
 	RECRD <- paste0("http://www.ipb-halle.de/ontologydata/record:",ACCES)
-	ASSAY <- paste0("http://www.ipb-halle.de/ontologydata/assay:",ACCES)
-	SPCTR <- paste0("http://www.ipb-halle.de/ontologydata/spectrum:",ACCES)
+	ASSAY <- paste0("http://www.ipb-halle.de/ontologydata/mass_spectrometry_assay:",ACCES)
+	SPCTR <- paste0("http://www.ipb-halle.de/ontologydata/mass_spectrum:",ACCES)
 		InchKeyIndex <- which(regexpr("INCHIKEY", Links, fixed=TRUE) == 1)
 		if(length(InchKeyIndex) > 0){
 	INCHK <- substring(Links[[InchKeyIndex]], 10)
-	CHENT <- paste0("http://www.ipb-halle.de/ontologydata/chemical-entity:",ACCES,"_",INCHK)
+	CHENT <- paste0("http://www.ipb-halle.de/ontologydata/chemical_entity:",ACCES,"_",INCHK)
 		} else{
 			return(NULL)
 		}
-		
+	
+
+	
+	
 	##PROPERTIES (between classes)
 	DESCR <- "http://www.ipb-halle.de/ontology/mbco#describes"
-	IDESC <- "http://www.ipb-halle.de/ontology/mbco#is-described-by"
-	OUTPU <- "http://www.ipb-halle.de/ontology/mbco#has-output"
-	IOUTP <- "http://www.ipb-halle.de/ontology/mbco#is-output-of"
+	IDESC <- "http://www.ipb-halle.de/ontology/mbco#is_described_by"
+	OUTPU <- "http://www.ipb-halle.de/ontology/mbco#has_output"
+	IOUTP <- "http://www.ipb-halle.de/ontology/mbco#is_output_of"
 	IDENT <- "http://www.ipb-halle.de/ontology/mbco#identifies"
-	IIDEN <- "http://www.ipb-halle.de/ontology/mbco#is-identified-by"
-	CPEAK <- "http://www.ipb-halle.de/ontology/mbco#contains-peak"
-	IPEAK <- "http://www.ipb-halle.de/ontology/mbco#is-peak-of"
+	IIDEN <- "http://www.ipb-halle.de/ontology/mbco#is_identified_by"
+	CPEAK <- "http://www.ipb-halle.de/ontology/mbco#has_constituent"
+	IPEAK <- "http://www.ipb-halle.de/ontology/mbco#constituates"
+	#CHEBI <- "http://www.ipb-halle.de/ontology/mbco#is_peak_of"
 	
 	##PROPERTIES (to strings, ints, etc.)
-	MZ <- "http://www.ipb-halle.de/ontology/mbco#has-mz"
-	INT <- "http://www.ipb-halle.de/ontology/mbco#has-int"
-	RELINT <- "http://www.ipb-halle.de/ontology/mbco#has-rel-int"
-	ACCESSION <- "http://www.ipb-halle.de/ontology/mbco#has-accession"
-	MSTYPE <- "http://www.ipb-halle.de/ontology/mbco#ms-type"
-	IONMODE <- "http://www.ipb-halle.de/ontology/mbco#ion-mode"
+	MZ <- "http://www.ipb-halle.de/ontology/mbco#encodes_mz"
+	INT <- "http://www.ipb-halle.de/ontology/mbco#has_intensity"
+	RELINT <- "http://www.ipb-halle.de/ontology/mbco#has_rel_intensity"
+	ACCESSION <- "http://www.ipb-halle.de/ontology/mbco#has_accession"
+	MSTYPE <- "http://www.ipb-halle.de/ontology/mbco#ms_type"
+	IONMODE <- "http://www.ipb-halle.de/ontology/mbco#ion_mode"
 	HASNAME <- "http://www.ipb-halle.de/ontology/mbco#name"
-	HASFORMULA <- "http://www.ipb-halle.de/ontology/mbco#has-formula"
-	HASSMILES <- "http://www.ipb-halle.de/ontology/mbco#has-smiles"
-	RECLINK <- "http://www.ipb-halle.de/ontology/mbco#hyperlink-record"
+	HASFORMULA <- "http://www.ipb-halle.de/ontology/mbco#has_formula"
+	HASSMILES <- "http://www.ipb-halle.de/ontology/mbco#has_smiles"
+	RECLINK <- "http://www.ipb-halle.de/ontology/mbco#hyperlink_record"
 	
 	##Generate Classes in the triple store and their relations + Accession and link to record in opendata(always the same)
 	TRIPLIST <- list()
@@ -53,15 +57,26 @@ EXTRACT <- function(record){
 	TRIPLIST[[6]] <- c(SPCTR,IOUTP,ASSAY)
 	TRIPLIST[[7]] <- c(SPCTR,IDENT,CHENT)
 	TRIPLIST[[8]] <- c(CHENT,IIDEN,SPCTR)
+	TRIPLIST[[9]] <- c(RECRD, "a" , "http://www.ipb-halle.de/ontology/mbco#record")
+	TRIPLIST[[10]] <- c(ASSAY, "a" , "http://www.ipb-halle.de/ontology/mbco#mass_spectrometry_assay")
+	TRIPLIST[[11]] <- c(SPCTR, "a" , "http://www.ipb-halle.de/ontology/mbco#mass_spectrum")
+	TRIPLIST[[12]] <- c(CHENT, "a" , "http://www.ipb-halle.de/ontology/mbco#chemical_entity")
 	
 	
-	Currnum <- 8
+	
+	Currnum <- 12
 	
 	##PEAKS
 	peaks <- w@compiled_ok[[1]][["PK$PEAK"]]
 	PEAKNAMES <- list()
 	for(i in 1:nrow(peaks)){
-		PEAKNAMES[[i]] <- paste0("http://www.ipb-halle.de/ontologydata/peak:",ACCES,"-",peaks[i,1])
+		##Find out peakname
+		PEAKNAMES[[i]] <- paste0("http://www.ipb-halle.de/ontologydata/peak:",ACCES,"_",peaks[i,1])
+		
+		##Write Class
+		Currnum <- Currnum + 1
+		TRIPLIST[[Currnum]] <- c(PEAKNAMES[[i]], "a", "http://www.ipb-halle.de/ontology/mbco#peak")
+		
 		Currnum <- Currnum + 1
 		TRIPLIST[[Currnum]] <- c(SPCTR,CPEAK,PEAKNAMES[[i]])
 		Currnum <- Currnum + 1
@@ -77,6 +92,10 @@ EXTRACT <- function(record){
 	
 	##ASSAY PART
 	MS <- w@compiled_ok[[1]][["AC$MASS_SPECTROMETRY"]]
+	
+	Currnum <- Currnum + 1
+	TRIPLIST[[Currnum]] <- c(ASSAY,IONMODE,MS$ION_MODE) 
+	
 	if(!is.na(MS$MS_TYPE)){
 		Currnum <- Currnum + 1
 		TRIPLIST[[Currnum]] <- c(ASSAY,MSTYPE,MS$MS_TYPE) 
@@ -103,7 +122,7 @@ EXTRACT <- function(record){
 
 require(rrdf)
 ret <- new.rdf()
-for(i in list.files("OpenData/IPB_Halle",full.names=TRUE)[201:400]){
+for(i in list.files("OpenData/IPB_Halle",full.names=TRUE)){
 	EXTLIST <- EXTRACT(i)
 	for(j in 1:length(EXTLIST)){
 		if(regexpr("http", EXTLIST[[j]][3], fixed=TRUE) == 1){
@@ -114,7 +133,7 @@ for(i in list.files("OpenData/IPB_Halle",full.names=TRUE)[201:400]){
 			
 	}
 }
-save.rdf(ret, "IPB2.xml","RDF/XML")
+save.rdf(ret, "IPB_full.N3","N3")
 
 
 ret2 <- new.rdf()
@@ -130,4 +149,3 @@ for(i in list.files("OpenData/UFZ",full.names=TRUE)[1:200]){
 		}
 	}
 }
-save.rdf(ret2, "UFZ.xml","RDF/XML")
